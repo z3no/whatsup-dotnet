ebe95bab2dc736aebe8161394bf43239ee66686c
Zeno Driesen <zeno.driesen@gmail.com>
2022-09-12 14:43:47 +0200

    basic string formatting in C# do have to say it all feels pretty natural, thanks to learning the other coding languages we have seen I guess

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -63,3 +63,47 @@ Console.Write(amount);
 Console.Write(" messages in your inbox. The temperature is ");
 Console.Write(temperature);
 Console.Write(" celsius.");
+
+//Character Escape Sequences and Verbatim Strings
+
+Console.WriteLine("Hello\nWorld!");
+Console.WriteLine("Hello\tWorld!");
+Console.WriteLine("Hello \"World\"!");
+//display a file path
+Console.WriteLine("c:\\source\\repos\n");
+
+Console.WriteLine("Generating invoices for costumer \"ABC Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.Write(@"c:\invoices");
+
+//To generate Japanese invoices:
+//Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+Console.WriteLine(@"c:\invoices\app.exe -j");
+
+//Verbatim String literal
+//use the @ directive before the literal string
+Console.Write("\n\n");
+Console.WriteLine(@"    c:\source\repos
+        (""this is where your code goes"")");
+
+//Unicode escape characters
+//Kon'nichiwa World
+Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
+
+//String concatenation
+string firstName = "Zeno";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
+
+//String interpolation
+string firstName = "Zeno";
+string greeting = "Hello";
+Console.WriteLine($"{greeting} {firstName}!");
+
+//Combine verbatim literals and string interpolation
+string projectName = "First-Project";
+Console.WriteLine($@"C:\\Output\{projectName}\Data");
+
